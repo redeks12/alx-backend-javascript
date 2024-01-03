52f263abacced4705982cfc526ff94c4f2ecf25a
kumdan job <redeks123456@gmail.com>
2024-01-03 21:55:00 +0100

    js promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,6 @@
+import { uploadPhoto, createUser } from "./utils.js";
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  return { photo, user };
+}
--- 0x01-ES6_promise/test.js
+++ 0x01-ES6_promise/test.js
@@ -1,13 +1,9 @@
-import guardrail from "./9-try.js";
-import divideFunction from "./8-try.js";
+import asyncUploadUser from "./100-await.js";
 
-console.log(
-  guardrail(() => {
-    return divideFunction(10, 2);
-  })
-);
-console.log(
-  guardrail(() => {
-    return divideFunction(10, 0);
-  })
-);
+const test = async () => {
+  const value = await asyncUploadUser();
+
+  console.log(value);
+};
+
+test();
