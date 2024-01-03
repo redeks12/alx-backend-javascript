15b90013c4760aa3522b12c993cbff5afd87b68d
kumdan job <redeks123456@gmail.com>
2024-01-03 18:32:13 +0100

    js promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(
       () => {
         return { status: 200, body: "success" };
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((x) => console.log(x[0].body, x[1].firstName, x[1].lastName))
     .catch(() => console.log("Signup system offline"));
 }
