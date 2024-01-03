cfc7ffc223b9f924bf5df83ce44e6fb056a9d251
kumdan job <redeks123456@gmail.com>
2024-01-03 18:39:00 +0100

    js promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((x) => console.log(x[0].body, x[1].firstName, x[1].lastName))
+    .then((x) => console.log(`${x[0].body} ${x[1].firstName} ${x[1].lastName}`))
     .catch(() => console.log("Signup system offline"));
 }
