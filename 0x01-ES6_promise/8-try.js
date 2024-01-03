f338b8025ad466a16b0886fb039df8e9b719d390
kumdan job <redeks123456@gmail.com>
2024-01-03 21:38:28 +0100

    js promises

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error("cannot divide by 0");
-    }
-    return numerator / denominator;
-  } catch (e) {
-    return e;
+  if (denominator === 0) {
+    throw new Error("cannot divide by 0");
   }
+  return numerator / denominator;
 }
