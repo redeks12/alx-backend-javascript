388b4242343ce2967be7a78d393eb0025df4d6d0
kumdan job <redeks123456@gmail.com>
2024-01-08 21:13:05 +0100

    es6 data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,5 +2,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length, position, value);
   const Intt = new Int8Array(buffer);
   Intt[position] = value;
-  return buffer;
+  return new DataView(buffer);
 }
