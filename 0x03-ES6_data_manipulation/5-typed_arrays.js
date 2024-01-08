10454108cf2c82a99bd73c4db2b1bd62b618d2c2
kumdan job <redeks123456@gmail.com>
2024-01-08 21:15:42 +0100

    es6 data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length) throw new Error("Position outside range");
   const buffer = new ArrayBuffer(length, position, value);
   const Intt = new Int8Array(buffer);
   Intt[position] = value;
