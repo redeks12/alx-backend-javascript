45ebc014f66713a7e1565434aeec43bdb7ed1b31
kumdan job <redeks123456@gmail.com>
2024-02-27 14:27:19 +0100

    m

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Hello Holberton School!\n");
+  res.end("Hello Holberton School!");
 });
 
 app.listen(1245, "localhost");
