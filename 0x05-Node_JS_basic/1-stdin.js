caf543a6d3f1a4cf10ab58a7781af5985c26b58b
kumdan job <redeks123456@gmail.com>
2024-02-27 13:29:37 +0100

    m

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.exit();
 });
-process.on("SIGINT", () => {
+process.on("exit", () => {
   process.stdout.write("This important software is now closing\n");
-  process.exit(0);
 });
