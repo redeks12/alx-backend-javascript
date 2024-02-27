4d3f05c5bd652aca35bdeb27a942c675d1c16e5f
kumdan job <redeks123456@gmail.com>
2024-02-27 13:41:45 +0100

    m

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,30 @@
 const fs = require("fs");
 function countStudents(path) {
-  fs.readFile(path, "utf-8", (err, data) => {
-    if (err) {
-      throw new Error("Cannot load the database");
-    }
-    const val = data.toString().trim().split("\n");
-    val.shift();
-    const stt = val.map((line) => {
-      return line.trim().split(",");
-    });
-    const dict = {};
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        throw new Error("Cannot load the database");
+      }
+      const val = data.toString().trim().split("\n");
+      val.shift();
+      const stt = val.map((line) => {
+        return line.trim().split(",");
+      });
+      const dict = {};
 
-    const fields = new Set(stt.map((x) => x[3]));
-    for (const field of fields) {
-      dict[field] = [];
-    }
-    stt.forEach((x) => {
-      dict[x[3]].push(x[0]);
+      const fields = new Set(stt.map((x) => x[3]));
+      for (const field of fields) {
+        dict[field] = [];
+      }
+      stt.forEach((x) => {
+        dict[x[3]].push(x[0]);
+      });
+      console.log(`Number of students: ${val.length}`);
+      Object.keys(dict).forEach((key) => {
+        console.log(`Number of students in ${key}: ${dict[key].join(", ")}`);
+      });
+      resolve(data);
     });
-    console.log(`Number of students: ${val.length}`);
-    Object.keys(dict).forEach((key) => {
-      console.log(`Number of students in ${key}: ${dict[key].join(", ")}`);
-    });
-  });
-  return new Promise((resolve) => {
-    resolve();
   });
 }
 module.exports = countStudents;
