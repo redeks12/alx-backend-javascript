0730e02433a4c6a52770711365d26476aa5f30b2
kumdan job <redeks123456@gmail.com>
2024-02-27 15:39:38 +0100

    m

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const port = 1245;
+const { readFile } = require("fs");
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = "";
+        const lines = data.toString().split("\n");
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(",");
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== "field") {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(", ")}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+app.get("/", (req, res) => {
+  res.end("Hello Holberton School!");
+});
+
+app.get("/students", (req, res) => {
+  res.write("This is the list of our students\n");
+  countStudents(process.argv[2].toString())
+    .then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    })
+    .catch(() => {
+      res.statusCode = 404;
+      res.end("Cannot load the database");
+    });
+});
+app.listen(port);
+module.exports = app;
