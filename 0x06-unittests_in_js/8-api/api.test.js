037f1e44f24e88202583882a2d5629ed3b5b6246
kumdan job <redeks123456@gmail.com>
2024-03-02 14:27:35 +0100

    unittest

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,9 +1,7 @@
 const expect = require("chai").expect;
 const request = require("request");
 const { describe, it } = require("mocha");
-const port = 7865;
-// const url = `http://localhost:${port}/`;
-// const vl = require("./api");
+
 describe("testing api request", () => {
   const options = {
     url: "http://localhost:7865/",
@@ -13,10 +11,8 @@ describe("testing api request", () => {
     request(options, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       expect(body).to.be.a("string");
+      expect(body).to.contain("Welcome to the payment system");
       expect(res.headers["content-length"]).to.equal("29");
-      //   console.log(res);
-      //   console.log(body);
-      //   console.log(err);
       done();
     });
   });
